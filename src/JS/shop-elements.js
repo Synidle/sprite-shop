4e94c83531ec57dc2c352fc99e1451cbcb7e2571
Synidle <73420926+Synidle@users.noreply.github.com>
2025-04-30 20:19:30 +0100

    product button base

---
+++ src/JS/shop-elements.js
@@ -0,0 +1,7 @@
+document.querySelector(".product-button").innerHTML = `
+    <button class="shop-item">
+        <img> <br>
+        <label class="item-name">Name of Product </label>
+        <label class="item-price">£14</label>
+    </button>
+`;
