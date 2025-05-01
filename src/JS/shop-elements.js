854dafee17670df86be718d74096252b52d4bdcd
Synidle <73420926+Synidle@users.noreply.github.com>
2025-05-01 21:39:18 +0100

    grid

--- src/JS/shop-elements.js
+++ src/JS/shop-elements.js
@@ -1,7 +1,9 @@
-document.querySelector(".product-button").innerHTML = `
-    <button class="shop-item">
-        <img> <br>
-        <label class="item-name">Name of Product </label>
-        <label class="item-price">£14</label>
-    </button>
-`;
+for (let productButton of document.querySelectorAll(".product-button")) {
+    productButton.innerHTML = `
+        <button class="shop-item">
+            <img> <br>
+            <label class="item-name">Name of Product </label>
+            <label class="item-price">£14</label>
+        </button>
+    `;
+}
